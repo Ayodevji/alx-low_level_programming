1251af05dfd6f02a063cf3cf132948a144045b9b
Hanif Abdulkareem <abdulkareemhanif@gmail.com>
2022-09-13 16:51:12 -0700

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ *  * main - A description
+ *   * Return: integer
+ */
+long int main(void)
+{
+printf('and that piece of art is useful" - Dora Korpar, 2015-10-19\\n');
+return (0);
+}
+
